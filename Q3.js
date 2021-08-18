a839e53f174e56412d66e85bb43a437d98a4229e
Rasna C V <49234026+rasnacv@users.noreply.github.com>
2021-08-18 20:00:14 +0530

    Update Q3.js

--- Q3.js
+++ Q3.js
@@ -7,7 +7,15 @@ company: 'Rohit'
 }
 var Emp1 = Object.create(employee);
 delete Emp1.company Console.log(emp1.company);
-*/
 
-//output
+
+OUTPUT
+-------
+undefined
+
+
+explanation
+------------
+'delete Emp1.company' will delete the key company from the object. So if we try to display it, output will be undefined as there is not such key now.
+*/
 
